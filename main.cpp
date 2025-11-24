f075fbd6bb2c81151d032adc90acd215aa91913b
Yuta Uchida <yuchida4dev@gmail.com>
2025-11-24 23:01:49 +0900

    minimal glfw setup with cmake

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+#include <GLFW/glfw3.h>
+
+void error_callback(int error, const char *description)
+{
+    fprintf(stderr, "GLFW Error: %s\n", description);
+}
+
+int main()
+{
+    // Set the error callback before initializing GLFW
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Set window hints for OpenGL 4.1 Core Profile
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Awesome Engine", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Clear the screen (black)
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap buffers and poll events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
