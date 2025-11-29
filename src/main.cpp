f256c6facf6210e6bfd0aa3a00ec61e9ee085866
Yuta Uchida <yuchida4dev@gmail.com>
2025-11-29 09:44:29 +0900

    first triangle

--- src/main.cpp
+++ src/main.cpp
@@ -69,23 +69,32 @@ int main()
         0.5f, -0.5f, 0.0f,
         0.0f, 0.5f, 0.0f};
 
-    unsigned int VBO;
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
     // load shader files
     std::string vertexShaderString = loadShaderAsString("shaders/simple.vert");
+    std::string fragShaderString = loadShaderAsString("shaders/simple.frag");
     const char *vertexShaderSource = vertexShaderString.c_str();
+    const char *fragShaderSource = fragShaderString.c_str();
 
-    unsigned int vertexShader;
+    unsigned int vertexShader, fragmentShader;
+    int success;
+    char infoLog[512];
+
+    // Compile vertext shader
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
-
-    // Check if compile went successfully
-    int success;
-    char infoLog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
 
     if (!success)
@@ -94,6 +103,38 @@ int main()
         std::cerr << "Error: Vertex Shader compilation failed\n"
                   << infoLog << std::endl;
     }
+
+    // Compile fragment shader
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cerr << "Error: Frag Shader compilation failed\n"
+                  << infoLog << std::endl;
+    }
+
+    // Link the compiled shaders to a program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cerr << "Error: Shader Program linking failed\n"
+                  << infoLog << std::endl;
+    }
+
+    // No longer needs shaders after attaching them to a program
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
     // Main loop
     while (!glfwWindowShouldClose(window))
@@ -108,6 +149,10 @@ int main()
         {
             glfwSetWindowShouldClose(window, true);
         }
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // Swap buffers and poll events
         glfwSwapBuffers(window);
